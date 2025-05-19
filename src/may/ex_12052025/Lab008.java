4c1352d7561dbc078d9a2f141ac1b24f40bd6734
Shruti0217-tester <shrutis0217@gmail.com>
2025-05-19 21:29:01 +0530

    This is 3rd Commit

---
+++ src/may/ex_12052025/Lab008.java
@@ -0,0 +1,13 @@
+package may.ex_12052025;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        System.out.println("Keywords and identifiers");
+        //keywords and Identifiers
+        // Reserved Words
+        // Simple English -> Pre Defined meaning in java programs.
+        //they are in blue color in intelliJ
+        //package, public, static, class, void are keywords
+        //Keywords are in lowercase
+    }
+}
---
+++ src/may/ex_12052025/Lab009.java
@@ -0,0 +1,13 @@
+package may.ex_12052025;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        //Variable and Datatypes, Identifiers
+        System.out.println("Datatypes");
+        boolean is_mohit_married = true;
+        boolean do_you_have_audi = false;
+        boolean do_you_have_bmw = true;
+        int age = 23;
+        float score = 92.34F;
+    }
+}
